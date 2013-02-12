84c154e892eaf2a84d5b1355e85a2b58f68f0be3
bryanyuan2 <bryanyuan2@gmail.com>
2013-02-12 21:28:38 +0800

    20130212

--- test.py
+++ test.py
@@ -3,9 +3,9 @@ from crf_confusion_matrix_class import crf_confusion_matrix_class
 def main():
 
 	correct_label_pool = ["I-NP","B-NP","B-ADJP","I-ADJP"]
-	filename = "final"
+	crf_test_result_filename = "crf_test_result"
 	
-	test = crf_confusion_matrix_class(correct_label_pool, filename)
+	test = crf_confusion_matrix_class(correct_label_pool, crf_test_result_filename)
 	test.confusion_matrix_print_func()
 
 if __name__ == "__main__":
